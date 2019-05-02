605ff3a260d3f24c80afa3b7f3643683999c54cd
Jean-Luc Aubert <jean-luc.a@web-projet.com>
2019-05-02 10:40:30 +0200

    Initial commit

---
+++ src/Entity/Article.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ArticleRepository")
+ */
+class Article
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $creationDate;
+
+    /**
+     * @ORM\Column(type="string", length=150)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="json", nullable=true)
+     */
+    private $content = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCreationDate(): ?\DateTimeInterface
+    {
+        return $this->creationDate;
+    }
+
+    public function setCreationDate(\DateTimeInterface $creationDate): self
+    {
+        $this->creationDate = $creationDate;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getContent(): ?array
+    {
+        return $this->content;
+    }
+
+    public function setContent(?array $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+}
