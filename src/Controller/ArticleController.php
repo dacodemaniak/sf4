85cd43005d40f63562399d1b0f295e6eeca05b6a
Jean-Luc Aubert <jean-luc.a@web-projet.com>
2019-05-02 12:16:27 +0200

    Remove symfony/serializer

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Article;
 
 class ArticleController extends AbstractController
 {
@@ -16,5 +19,58 @@ class ArticleController extends AbstractController
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/ArticleController.php',
         ]);
+    }
+
+    /**
+     * @Route("/article/add", methods={"POST", "HEAD"}, name="add-article")
+     */
+    public function add(): Response {
+        $article = new Article();
+        $article
+            ->setCreationDate(new \DateTime())
+            ->setTitle("Premier article")
+            ->setContent(
+                [
+                    "abstract" => "Troisième article",
+                    "content" => "Try to get the serialization",
+                    "see" => "https://docs.symfony.com/serialization"
+                ]
+            );
+        $repository = $this->getDoctrine()->getManager();
+        $repository->persist($article);
+        $repository->flush();
+        
+        return new Response(
+            $this->json(["id" => $article->getId()]), 
+            Response::HTTP_CREATED,
+            []
+        );
+    }
+    
+    /**
+     * @Route("/article/{id}", methods={"PUT", "HEAD"}, name="upd-article")
+     */
+    public function update(Request $request): Response {
+        // Chercher l'article à partir de l'ID passé en paramètre
+        $repository = $this->getDoctrine()->getRepository(Article::class);
+        $article = $repository->find($request->get("id"));
+        
+        if ($article) {
+            $article->setTitle("Titre du troisième article");
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($article);
+            $entityManager->flush();
+            
+            return new Response(
+                $this->json(["id" => $article->getId()]),
+                Response::HTTP_ACCEPTED
+            );
+        } else {
+            return new Response(
+                "L'article : " . $request->get("id") . " n'existe plus !",
+                Response::HTTP_NOT_MODIFIED
+             );
+        }
+
     }
 }
